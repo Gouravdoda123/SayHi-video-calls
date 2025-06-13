5c902992dc53b6e625c5f28c9815cfe357c45cb3
Gouravdoda123 <gouravdoda123@gmail.com>
2025-06-14 00:44:23 +0530

    🔧 Updated auth routes, middleware, and password reset flow

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,7 +3,8 @@ import User from "../models/User.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.accessToken;
+
 
     if (!token) {
       return res.status(401).json({ message: "Unauthorized - No token provided" });
